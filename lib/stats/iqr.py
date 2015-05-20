054150de7385907001f3f26d0befaf2764fa6753
xmn <ivan.ogassawara@gmail.com>
2015-05-19 21:55:42 -0300

    actualizando docstring

--- lib/stats/iqr.py
+++ lib/stats/iqr.py
@@ -8,7 +8,6 @@ def iqr(data: pd.Series, min_value=None) -> (float, float):
     IQR: Q3-Q1
     Outlier: Q3 + 1.5*(IQR) > DATA < Q1 - 1.5*IQR
 
-    :param data:
     :param data: pd.Series
     :return: mask considering lower bound and upper bound
 
