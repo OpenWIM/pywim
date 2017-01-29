ba17a3e04ff64a3ad39f9d0b35f582dd4cc13748
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-01-29 21:36:19 -0200

    Data variable problem fixed.

--- pywim/utils/dsp/wave_curve.py
+++ pywim/utils/dsp/wave_curve.py
@@ -9,24 +9,26 @@ def select_curve_by_threshold(
     """
 
     """
+    Δx = delta_x
+
     indexes = peakutils.indexes(signal_data, thres=0.5, min_dist=30)
     curves = []
 
     for ind_axle in indexes:
-        i_start = ind_axle - delta_x
-        i_end = ind_axle + delta_x
+        i_start = ind_axle - Δx
+        i_end = ind_axle + Δx
 
         p_start = Δx
         while i_start >= p_start:
             i_start -= 1
-            if data[k].iloc[i_start] <= threshold:
+            if signal_data.iloc[i_start] <= threshold:
                 break
 
-        p_end = signal_data.size - delta_x
+        p_end = signal_data.size - Δx
         while i_end <= p_end:
             i_end += 1
-            if data[k].iloc[i_end] <= threshold:
+            if signal_data.iloc[i_end] <= threshold:
                 break
-        curves.append(data[k].iloc[i_start - delta_x:i_end + delta_x])
+        curves.append(signal_data.iloc[i_start - Δx:i_end + Δx])
 
     return curves
